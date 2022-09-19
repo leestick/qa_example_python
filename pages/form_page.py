33728eaf2482e916ae5680c602d7ac5559b942d8
ioK <leestick@mail.ru>
2022-09-19 22:50:26 +0300

    ssh

---
+++ pages/form_page.py
@@ -0,0 +1,7 @@
+"""methods, working with web-pages"""
+from pages.base_page import BasePage
+from locators.form_page_locators import FormPageLocators as Locators
+class FormPage(BasePage):
+
+    def fill_fields_and_submit(self):
+        pass
